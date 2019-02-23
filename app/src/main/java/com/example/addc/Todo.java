8207fb3d6059dbe6e1308c357382b0482a9c1135
--global <pinkappleberry98@gmail.com>
2019-02-23 17:35:39 +0700

    create todo activity

---
+++ app/src/main/java/com/example/addc/Todo.java
@@ -0,0 +1,11 @@
+package com.example.addc;
+
+import java.io.Serializable;
+
+public class Todo implements Serializable {
+
+    private String name;
+    private String description;
+    private String 
+    private String deadline;
+}
