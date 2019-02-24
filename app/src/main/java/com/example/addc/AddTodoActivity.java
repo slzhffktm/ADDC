dac4186021b88c901c2b8778c5bba45412cb1c27
--global <pinkappleberry98@gmail.com>
2019-02-24 20:49:34 +0700

    create spinner for matakuliah on add todo

--- app/src/main/java/com/example/addc/AddTodoActivity.java
+++ app/src/main/java/com/example/addc/AddTodoActivity.java
@@ -2,6 +2,7 @@ package com.example.addc;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,21 +12,33 @@ import android.widget.EditText;
 import android.widget.TimePicker;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+
+import in.galaxyofandroid.spinerdialog.OnSpinerItemClick;
+import in.galaxyofandroid.spinerdialog.SpinnerDialog;
 
 public class AddTodoActivity extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
+    private DatabaseReference table_matakuliah;
     final Calendar calendar = Calendar.getInstance();
 
     EditText editTextDueDate;
     EditText editTextDueTime;
+    EditText editTextMataKuliah;
 
     DatePickerDialog datePickerDialog;
     TimePickerDialog timePickerDialog;
+
+    ArrayList<String> stringMataKuliahs = new ArrayList<>();
+    SpinnerDialog spinnerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +46,45 @@ public class AddTodoActivity extends AppCompatActivity {
         FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_add_todo);
 
+        table_matakuliah = FirebaseDatabase.getInstance().getReference("mata_kuliahs");
+        table_matakuliah.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds: dataSnapshot.getChildren()) {
+                    try {
+                        stringMataKuliahs.add(ds.getKey() + " " + ds.child("name").getValue().toString());
+                    } catch (Exception e) {
+                        Log.w("RETRIEVE DATA", "name empty");
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w("RETRIEVE DATA", "retrieving data error");
+            }
+        });
+
+        editTextMataKuliah = findViewById(R.id.input_matakuliah);
         editTextDueDate = findViewById(R.id.input_duedate);
         editTextDueTime = findViewById(R.id.input_duetime);
+
+        editTextMataKuliah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                spinnerDialog = new SpinnerDialog(AddTodoActivity.this, stringMataKuliahs,
+                        "Select Mata Kuliah", R.style.DialogAnimations_SmileWindow,"Cancel");
+                spinnerDialog.bindOnSpinerListener(new OnSpinerItemClick() {
+                    @Override
+                    public void onClick(String item, int position) {
+                        editTextMataKuliah.setText(item);
+                    }
+                });
+                spinnerDialog.setCancellable(true); // for cancellable
+                spinnerDialog.setShowKeyboard(false);// for open keyboard by default
+                spinnerDialog.showSpinerDialog();
+            }
+        });
 
         editTextDueDate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,7 +115,5 @@ public class AddTodoActivity extends AppCompatActivity {
                 timePickerDialog.show();
             }
         });
-
-        mDatabase = FirebaseDatabase.getInstance().getReference();
     }
 }
