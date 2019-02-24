7f2f5187d02529244a0b3e21235bad078ce8c00c
Virmith <13516049@std.stei.itb.ac.id>
2019-02-24 13:41:15 +0700

    Merge branch 'master' of https://github.com/slzhffktm/ADDC into Limit
    # Conflicts:
    #	build.gradle

--- app/src/main/java/com/example/addc/MataKuliah.java
+++ app/src/main/java/com/example/addc/MataKuliah.java
@@ -1,5 +1,31 @@
 package com.example.addc;
 
 public class MataKuliah {
+    private String id;
+    private String nama;
 
+    public MataKuliah(){
+
+    }
+
+    public MataKuliah(String id, String nama) {
+        this.id = id;
+        this.nama = nama;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
 }
