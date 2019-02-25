4c7105412085530a1be2e4e00b5184156d791bbd
Ellen Nugroho <ellennugroho9@gmail.com>
2019-02-25 16:38:11 +0700

    background service for sending location to database

--- app/src/main/java/com/example/addc/MainActivity.java
+++ app/src/main/java/com/example/addc/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.addc;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -16,6 +21,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -33,6 +39,7 @@ public class MainActivity extends AppCompatActivity
 //    SharedPreferences.Editor editor;
 //    private boolean isUserLoggedIn;
     GoogleSignInClient mGoogleSignInClient;
+    private static final int PERMISSIONS_REQUEST = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -95,6 +102,26 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        // Start code
+        LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            finish();
+        }
+
+        //Check whether this app has access to the location permission//
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
+        //If the location permission has been granted, then start the TrackerService//
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            startTrackerService();
+        } else {
+            //If the app doesn’t currently have access to the user’s location, then request access//
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST);
+        }
+        // End code
     }
 
     @Override
@@ -133,6 +160,13 @@ public class MainActivity extends AppCompatActivity
             finish();
         }
         super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+//        unregisterReceiver();
     }
 
     @Override
@@ -229,4 +263,28 @@ public class MainActivity extends AppCompatActivity
         startActivity(intent);
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[]
+            grantResults) {
+        //If the permission has been granted...//
+        if (requestCode == PERMISSIONS_REQUEST && grantResults.length == 1
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            //...then start the GPS tracking service//
+            startTrackerService();
+        } else {
+            //If the user denies the permission request, then display a toast with some more information//
+            Toast.makeText(this, "Please enable location services to allow GPS tracking", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //Start the TrackerService//
+
+    private void startTrackerService() {
+        startService(new Intent(this, TrackingService.class));
+        //Notify the user that tracking has been enabled//
+        Toast.makeText(this, "GPS tracking enabled", Toast.LENGTH_SHORT).show();
+    }
+
 }
---
+++ app/src/main/java/com/example/addc/TrackingService.java
@@ -0,0 +1,115 @@
+package com.example.addc;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import android.Manifest;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.Uri;
+import android.os.IBinder;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+public class TrackingService extends Service {
+    private static final String TAG = TrackingService.class.getSimpleName();
+
+    public TrackingService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        buildNotification();
+        requestLocationUpdates();
+    }
+
+    //Create the persistent notification//
+
+    private void buildNotification() {
+        String stop = "stop";
+        registerReceiver(stopReceiver, new IntentFilter(stop));
+        PendingIntent broadcastIntent = PendingIntent.getBroadcast(
+                this, 0, new Intent(stop), PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Create the persistent notification//
+        Notification.Builder builder = new Notification.Builder(this)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText(getString(R.string.tracking_enabled_notif))
+
+        //Make this notification ongoing so it can’t be dismissed by the user//
+                .setOngoing(true)
+                .setContentIntent(broadcastIntent)
+                .setSmallIcon(R.drawable.tracking_enabled);
+        startForeground(1, builder.build());
+    }
+
+    protected BroadcastReceiver stopReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            //Unregister the BroadcastReceiver when the notification is tapped//
+            unregisterReceiver(stopReceiver);
+
+            //Stop the Service//
+            stopSelf();
+        }
+    };
+
+    private void requestLocationUpdates() {
+        LocationRequest request = new LocationRequest();
+
+        //Specify how often your app should request the device’s location//
+        request.setInterval(10000);
+
+        //Get the most accurate location data available//
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(this);
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
+        //If the app currently has access to the location permission...//
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            //...then request location updates//
+            client.requestLocationUpdates(request, new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    //Get a reference to the database, so your app can perform read and write operations//
+                    DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+                    Location location = locationResult.getLastLocation();
+                    if (location != null) {
+                        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(TrackingService.this);
+                        Log.d("FirebaseUser", "onLocationResult: "+acct);
+                        String personId = acct.getId();
+                        Log.d("FirebaseUser", "ID: "+personId);
+
+//                        Save the location data to the database//
+                        ref.child("users").child(personId).child("latitude").setValue(location.getLatitude());
+                        ref.child("users").child(personId).child("longitude").setValue(location.getLongitude());
+                    }
+                }
+            }, null);
+        }
+    }
+}
